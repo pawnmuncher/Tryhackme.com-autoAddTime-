641e087bd3c86fa827d6b95e12163a320b85c469
pawnmuncher <>
2021-09-24 14:33:40 -0500

    added tryhackme autoAddTime javascript to add time without me having to remember.

---
+++ tryHackMe_autoAddTime.js
@@ -0,0 +1,10 @@
+// Author : pawnmuncher
+// A little javascript to automatically add 1 hour your active Tryhackme.com machine/lab.
+// Simply set the desired interval for the script to autoAddTime to the machine, 
+// then paste the code in browsers console window using the developers tools
+
+setInterval(autoAddTime, 270000); // minutes in milliseconds (45 minutes = 2700000ms), set to 5000 or 5 seconds to test if you like ;)
+
+function autoAddTime() {
+  $("#add-vm-time").click();
+}
