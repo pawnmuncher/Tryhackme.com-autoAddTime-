1db0c1f22c07168fbe1f302e1e377d188e308758
pawnmuncher <>
2021-09-24 16:24:13 -0500

    correcting spelling

--- tryHackMe_autoAddTime.js
+++ tryHackMe_autoAddTime.js
@@ -3,7 +3,7 @@
 // Simply set the desired interval for the script to autoAddTime to the machine, 
 // then paste the code in browsers console window using the developers tools
 
-setInterval(autoAddTime, 270000); // minutes in milliseconds (45 minutes = 2700000ms), set to 5000 or 5 seconds to test if you like ;)
+setInterval(autoAddTime, 2700000); // minutes in milliseconds (45 minutes = 2700000ms), set to 5000 or 5 seconds to test if you like ;)
 
 function autoAddTime() {
   $("#add-vm-time").click();
